87cd4ec620caa611d89119c606c29d183086efb8
DavideGiuseppeCiliento <cilientodg@gmail.com>
2025-04-23 16:56:45 +0200

    Esercizio completato

--- js/script.js
+++ js/script.js
@@ -21,15 +21,22 @@ const list = document.querySelector("ul");
 generationEmails.addEventListener("click", () => {
   let listEmailsHTML = "";
   const emails = [];
-
+  console.log("PRIMA DEL FOR");
   for (let i = 0; i < 10; i++) {
     axios.get("https://flynn.boolean.careers/exercises/api/random/mail").then((response) => {
       emails.push(response.data.response);
       console.log(emails[i]);
+
+      console.log("DENTRO AL THEN", emails[i]);
       listEmailsHTML += `<li>${emails[i]}</li>`;
-      stampa(listEmailsHTML);
+
+      if (emails.length === 10) {
+        stampa(listEmailsHTML);
+      }
     });
   }
+
+  console.log("DOPO IL FOR");
 });
 
 const stampa = (listEmailsHTML) => {
