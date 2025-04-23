e503ddb1a5fb6e247386e9cbbbae63ca2e078a50
DavideGiuseppeCiliento <cilientodg@gmail.com>
2025-04-23 15:13:07 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,5 @@
+// # ACQUISIZIONE API
+axios.get("https://flynn.boolean.careers/exercises/api/https://flynn.boolean.careers/exercises/api/random/mail/int").then((response) => {
+  const email = response.data.response;
+  console.log(email);
+});
