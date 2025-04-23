4b770f4539923a8872267932d1c4799d6cd9e257
DavideGiuseppeCiliento <cilientodg@gmail.com>
2025-04-23 16:29:18 +0200

    Fatto ma problemi nello stampare tutto insieme

--- js/script.js
+++ js/script.js
@@ -1,42 +1,37 @@
-// // # ACQUISIZIONE API
-// axios.get("https://flynn.boolean.careers/exercises/api/random/mail").then((response) => {
-//   for (let i = 0; i < 10; i++) {
-//     emails.push(response.data.response);
-//   }
-//   console.table(emails);
-// });
-
 // const generationEmails = document.getElementById("generation-emails");
 // const list = document.querySelector("ul");
-// let listEmailsHTML = "";
 
 // generationEmails.addEventListener("click", () => {
+//   let listEmailsHTML = "";
+//   const emails = [];
+
 //   for (let i = 0; i < 10; i++) {
 //     axios.get("https://flynn.boolean.careers/exercises/api/random/mail").then((response) => {
 //       emails.push(response.data.response);
-//       listEmailsHTML += `<li>${emails[i]}</li>`;
-
 //       console.log(emails[i]);
+//       listEmailsHTML += `<li>${emails[i]}</li>`;
+//       list.innerHTML = listEmailsHTML;
 //     });
 //   }
-//   console.table(emails);
-//   console.table(listEmailsHTML);
 // });
-
-// list.innerHTML = listEmailsHTML;
 
 const generationEmails = document.getElementById("generation-emails");
 const list = document.querySelector("ul");
-let listEmailsHTML = "";
-const emails = [];
 
 generationEmails.addEventListener("click", () => {
+  let listEmailsHTML = "";
+  const emails = [];
+
   for (let i = 0; i < 10; i++) {
     axios.get("https://flynn.boolean.careers/exercises/api/random/mail").then((response) => {
       emails.push(response.data.response);
       console.log(emails[i]);
       listEmailsHTML += `<li>${emails[i]}</li>`;
+      stampa(listEmailsHTML);
     });
   }
-  list.innerHTML = listEmailsHTML;
 });
+
+const stampa = (listEmailsHTML) => {
+  list.innerHTML = listEmailsHTML;
+};
